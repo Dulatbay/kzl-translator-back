a4d810e7854f369ebb4928f67faa5accf62e4613
akhan <akhan.dulatbay@gmail.com>
2024-11-25 08:35:48 +0500

    initial commit

---
+++ assistant.js
@@ -0,0 +1,95 @@
+require('dotenv').config();
+
+
+// singleton
+const {OpenAI} = require('openai');
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const assistantId = process.env.ASSISTANT_ID;
+
+class Assistant {
+    static assistant = null
+    static thread = null;
+
+    async #getAssistant() {
+        if (!Assistant.assistant)
+            Assistant.assistant = openai.beta.assistants.retrieve(assistantId)
+        return Assistant.assistant;
+    }
+
+    async #getThread() {
+        if (!Assistant.thread)
+            Assistant.thread = await openai.beta.threads.create();
+        return Assistant.thread;
+    }
+
+    async sendMessage(msg) {
+        const thread = await this.#getThread();
+        const assistant = await this.#getAssistant();
+
+        console.log('Assistant fetched', assistant);
+        console.log('Thread fetched', thread);
+
+
+        const message = await openai.beta.threads.messages.create(
+            thread.id,
+            {
+                role: "user",
+                content: msg
+            }
+        );
+
+        let run = await openai.beta.threads.runs.createAndPoll(
+            thread.id,
+            {
+                assistant_id: assistant.id,
+            }
+        );
+
+        if (run.status === 'completed') {
+            const messages = await openai.beta.threads.messages.list(
+                run.thread_id
+            );
+
+            return messages.data[0];
+        } else {
+            console.log(run)
+        }
+    }
+
+    async sendMessageWithStreaming(msg, res) {
+        const thread = await this.#getThread();
+        const assistant = await this.#getAssistant();
+
+        console.log('Assistant fetched', assistant);
+        console.log('Thread fetched', thread);
+
+        let finalResponse = '';
+
+        const message = await openai.beta.threads.messages.create(
+            thread.id,
+            {
+                role: "user",
+                content: msg
+            }
+        );
+
+        const run = openai.beta.threads.runs.stream(thread.id, {
+            assistant_id: assistant.id
+        })
+            .on('textDelta', (textDelta, snapshot) => {
+                finalResponse += textDelta.value;
+            })
+            .on('end', () => {
+                res.send(finalResponse);
+            })
+            .on('error', (err) => {
+                console.error('Error while streaming:', err);
+                res.status(500).send('Something went wrong while streaming!');
+            });
+    }
+}
+
+module.exports = Assistant;
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const Assistant = require("./assistant");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors())
+
+let assistant = new Assistant();
+
+
+
+app.post('/translate', async (req, res) => {
+    try {
+        const {msg} = req.body
+
+        const message = await assistant.sendMessage(msg)
+
+        if(!res)
+            res.status(500).send('Something went wrong');
+        else {
+            res.status(200).send(message);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Something went wrong!');
+    }
+});
+
+
+app.post('/translate-stream', async (req, res) => {
+    try {
+        const {msg} = req.body
+
+        await assistant.sendMessageWithStreaming(msg, res)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Something went wrong!');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
